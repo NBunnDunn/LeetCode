b261547f357817019d8912d4b7b45e4ecf53065c
Nick Dunn <nickdunn99@gmail.com>
2023-01-03 08:34:38 -0700

    Addded another solution to 1/3/23.

--- Jan_23/3_Jan_23.js
+++ Jan_23/3_Jan_23.js
@@ -80,3 +80,27 @@ divide = function (dividend, divisor) {
 };
 
 const result = divide(53, 3);
+
+// ==== Submission 2 (Java) ====
+// Date: 1/3/23
+// Runtime: 2ms (faster than 65.16%)
+// Memory: 42mb (less than 12.55%)
+
+// class Solution {
+//     public int divide(int dividend, int divisor) {
+//         int sign,result=0,value=31;
+// 		if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) sign = 1;  else sign = -1;
+// 		long ldividend = Math.abs((long) dividend);
+// 		long ldivisor = Math.abs((long) divisor);
+// 		while (ldividend >= ldivisor) {
+// 			while (ldividend < ldivisor << value) {
+// 				value--;
+// 			}
+// 			ldividend -= ldivisor << value;
+// 			result += 1 << value;
+// 		}
+// 		if (result > Integer.MAX_VALUE) result = Integer.MAX_VALUE;
+// 		if(dividend==Integer.MIN_VALUE && divisor==-1) result=Integer.MAX_VALUE;
+//         result= sign > 0 ? result : -result; return result;
+//     }
+// }
